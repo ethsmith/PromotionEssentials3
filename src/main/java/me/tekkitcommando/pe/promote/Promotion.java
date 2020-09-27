9bd8da73271aa5d9ce708997b83a11ac959c94ed
ethsmith <ethan.smith1845@gmail.com>
2020-09-27 11:40:16 -0500

    Add command processing

--- src/main/java/me/tekkitcommando/pe/promote/Promotion.java
+++ src/main/java/me/tekkitcommando/pe/promote/Promotion.java
@@ -1,8 +1,13 @@
 package me.tekkitcommando.pe.promote;
 
+import me.tekkitcommando.pe.PromotionEssentials;
+import me.tekkitcommando.pe.data.DataManager;
 import me.tekkitcommando.pe.permission.PermissionManager;
 import net.milkbowl.vault.permission.Permission;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class Promotion {
 
@@ -11,5 +16,20 @@ public class Promotion {
         Permission permissions = PermissionManager.getPermissions();
         permissions.playerRemoveGroup(player, permissions.getPrimaryGroup(player));
         permissions.playerAddGroup(null, player, rank);
+
+        List<String> consoleCommands = DataManager.getConfig().getStringList("commands." + rank + ".console");
+        List<String> playerCommands = DataManager.getConfig().getStringList("commands." + rank + ".player");
+
+        if (consoleCommands != null) {
+            PromotionEssentials plugin = PromotionEssentials.getInstance();
+            ConsoleCommandSender console = plugin.getServer().getConsoleSender();
+            for (String command : consoleCommands)
+                plugin.getServer().dispatchCommand(console, command.replace("%player%", player.getName()));
+        }
+
+        if (playerCommands != null) {
+            for (String command : playerCommands)
+                player.performCommand(command);
+        }
     }
 }
